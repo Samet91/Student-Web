085a8bc7028f427f5449c8c59704b7af53f6d30a
Samet Uslu <samet_u@hotmail.de>
2022-02-16 10:32:06 +0100

    create jUnit tests

---
+++ src/test/java/de/uslu/studentweb/ShopServiceTest.java
@@ -0,0 +1,54 @@
+package de.uslu.studentweb;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopServiceTest {
+
+    @Test
+    void ShouldAddOneProduct() {
+
+        Product product = new Product("Banane", "1");
+        HashMap<String, Product> map = new HashMap<>();
+        ProductRepo productsRepo = new ProductRepo(map);
+        OrderRepo orders = new OrderRepo();
+
+        ShopService newShopService = new ShopService(orders, productsRepo);
+        newShopService.addProduct(product);
+        var result = newShopService.getProduct("1");
+        assertEquals(product, result.get());
+    }
+
+    @Test
+    void shouldAddProduct() {
+
+        Product product = new Product("Banane", "1");
+        HashMap<String, Product> map = new HashMap<>();
+        assertEquals(map.size(), 0);                    // liste/map ist leer um zu beweisen ob das adden funktioniert
+        ProductRepo productsRepo = new ProductRepo(map);
+
+        ShopService newShopService = new ShopService(new OrderRepo(), productsRepo);
+        newShopService.addProduct(product);                                 //hier object hinzugefügt beweis dass adden klappt.
+        var result = newShopService.getProduct("1");
+        assertEquals(product, result.get());
+    }
+
+    @Test
+    void shouldReturnProductList() {
+        Product product1 = new Product("ads", "1");
+        Product product2 = new Product("da", "2");
+        Product product3 = new Product("asdsa", "3");
+        HashMap<String, Product> productsList = new HashMap<>();
+        ProductRepo productRepo = new ProductRepo(productsList);
+        productRepo.add(product1);
+        productRepo.add(product2);
+        productRepo.add(product3);
+        ShopService shopService = new ShopService(new OrderRepo(), productRepo);
+        assertEquals(3, shopService.listProducts().size());
+
+    }
+
+}
