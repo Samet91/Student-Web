bc44b518814d74f9ad470cc2293d67bee5472e39
Samet Uslu <samet_u@hotmail.de>
2022-02-17 18:27:17 +0100

    empty

--- src/main/java/de/uslu/studentweb/github/GithubService.java
+++ src/main/java/de/uslu/studentweb/github/GithubService.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GithubService {
     public Repository[] getRepositories(String githubUser) {
+
         String url = "https://api.github.com/users/" + githubUser + "/repos";
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<Repository[]> response = restTemplate.getForEntity(url, Repository[].class);
