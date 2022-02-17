04d814a66880e8a8a6b23639483c773984915735
Samet Uslu <samet_u@hotmail.de>
2022-02-17 16:24:56 +0100

    change for loop to Arrow function

--- src/main/java/de/uslu/studentweb/github/GithubController.java
+++ src/main/java/de/uslu/studentweb/github/GithubController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/github")
 public class GithubController {
@@ -19,13 +21,12 @@ public class GithubController {
 
     @GetMapping("/{githubUser}")
     public String[] getRepositoryNames(@PathVariable String githubUser) {
-
         Repository[] repositories = githubService.getRepositories(githubUser);
 
-        String[] repositoryNames = new String[repositories.length];
-        for (int i=0; i < repositoryNames.length; i++) {
-            repositoryNames[i] = repositories[i].getName();
-        }
-        return repositoryNames;
+        String[] repoName = Arrays.stream(repositories)
+                .map(Repository::getName)
+                .toArray(String[]::new);
+
+         return repoName;
     }
 }
